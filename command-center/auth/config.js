2a0ef49884e901c1ae69d2e0f594951d82289a7d
GrupoMauricio <agencianextlevelbr@gmail.com>
2026-07-29 04:07:23 -0300

    feat: configura conexão pública com Supabase

---
+++ command-center/auth/config.js
@@ -0,0 +1,7 @@
+window.COMMAND_CENTER_CONFIG = {
+  supabaseUrl: "https://axuayfqvvwdrjrhfhhpf.supabase.co",
+  supabaseAnonKey: "sb_publishable_ZLdepxOd0PenmIu__BSS3w_lfMvQbrR",
+  organizationSlug: "grupo-mauricio",
+  locale: "pt-BR",
+  timezone: "America/Sao_Paulo"
+};
